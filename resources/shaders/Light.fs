b461d0510007daf8b5aae92cc517e1182c2f822e
jj-jc <juanjosejuradocamino@gmail.com>
2022-08-08 12:10:57 +0200

    Added the basic of colors and Phong Model Shading (lightning

---
+++ resources/shaders/Light.fs
@@ -0,0 +1,30 @@
+#version 330 core
+
+in vec3 fragmentPos;
+in vec3 fragmentNorm;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+out vec4 fragmentColor;
+
+void main()
+{
+    // ambient lightning
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor; 
+    // diffuse lightning
+    vec3 lightDir = normalize(lightPos - fragmentPos);
+    vec3 diffuse = max(dot(fragmentNorm, lightDir), 0.0f) * lightColor; 
+    // specular lightning
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - fragmentPos);
+    vec3 reflectionDir = reflect(-lightDir, fragmentNorm);
+    vec3 specular = specularStrength * pow(max(dot(viewDir, reflectionDir), 0), 32) * lightColor; // shininess of n = 32 higher -> brighter (2, 4, 8, 16, 32, 64, 128, 256);
+
+    // apply the lightning result with the 3 components
+    vec3 result = (specular + ambient + diffuse) * objectColor;
+    fragmentColor = vec4(result, 1.0);
+}
