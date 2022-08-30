bfc3cf9b48f217a3a8faa6b79ab63da2a63cfeb2
jj-jc <juanjosejuradocamino@gmail.com>
2022-08-30 16:27:04 +0200

    Added a way to Render Text

---
+++ TextRendering/src/main.cpp
@@ -0,0 +1,401 @@
+// Include of the GLEW. always first than the glfw3
+#include <GL/glew.h>
+
+// Include of the GLFW
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// #include "internal/filesystem.h"
+#include "internal/shader.h"
+#include "internal/camera.h"
+#include "internal/model.h"
+
+#include <iostream>
+
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+// Debug 
+
+// #################################################
+// ############ Global variables ###################
+// #################################################
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+// camera system variables
+Camera camera(glm::vec3(0.0f, 0.0f, 0.0f));
+// mouse controllers
+float lastX = (float)SCR_WIDTH/2;
+float lastY = (float)SCR_HEIGHT/2;
+bool firstMouse = true;
+// Time management
+double deltaTime = 0.0f; // Time between current frame and last frame
+double lastFrame = 0.0f;  // Time of last frame
+
+// #################################################
+// ############ Prototypes ###################
+// #################################################
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void loadGPUTexture(const char* path, unsigned int &textureID, GLenum textureUnit);
+void RenderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color);
+// Debug 
+GLenum glCheckError_(const char* file, int line);
+void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam);
+
+// FreeType
+// --------------
+struct Character{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::vec2 Size; // Size of glyph
+    glm::vec2 Bearing;  // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Offset to advance to next glyph
+};
+std::map<char, Character> Characters;
+unsigned int VAO, VBO;
+
+
+
+int main()
+{
+    // start GL context and O/S window using the GLFW helper library
+    if (!glfwInit()) {
+      fprintf(stderr, "ERROR: could not start GLFW3\n");
+      return 1;
+    } 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Dont know for what do this
+    #ifdef MY_DEBUG
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true); // requesting a debug context //TODO: COMMENT THIS LINE IN RELEASE BUILD   
+        std::cout << "Created a debug context\n";
+    #endif
+    /* Create a windowed mode window and its OpenGL context */
+    // ------------------------------------------------------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MyOpenGL", NULL, NULL);
+    if (!window)
+    {
+    	std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // hide the cursor and capture it
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glCheckError();
+    /* This has to be done after make a valid OpenGL rendering context */
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error with the glewInit" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    // Debug output
+    // -----------------
+    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if(flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        // Initialize debug output
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    }
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // Freetype library process
+    // ---------------------
+    FT_Library ft; // Init
+    if(FT_Init_FreeType(&ft))
+        glDebugMessageInsert(GL_DEBUG_SOURCE_THIRD_PARTY, GL_DEBUG_TYPE_ERROR, 0, GL_DEBUG_SEVERITY_HIGH, -1, "ERROR::FREETYPE: Could not init FreeType Library");
+
+    FT_Face face; // create a face where store the source
+    if(FT_New_Face(ft, "/home/jjjurado/Dev/MyOpenGL/resources/fonts/Antonio-Light.ttf", 0, &face))
+        glDebugMessageInsert(GL_DEBUG_SOURCE_THIRD_PARTY, GL_DEBUG_TYPE_ERROR, 0, GL_DEBUG_SEVERITY_HIGH, -1, "ERROR::FRETYPE: Failed to load font");
+    FT_Set_Pixel_Sizes(face, 0, 48); // define the size in pixels
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // No byte-aligment restriction
+
+    for( unsigned int c = 0; c < 182; c++)
+    { // loop for store the 182 characters in ASCII
+        if(FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            glDebugMessageInsert(GL_DEBUG_SOURCE_THIRD_PARTY, GL_DEBUG_TYPE_ERROR, 0, GL_DEBUG_SEVERITY_MEDIUM, -1, "ERROR::FRETYPE: Failed to load Glyph");
+            continue;
+        }
+        // generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+        Characters.insert(std::pair<char, Character>(c, character));
+    }
+
+    glEnable(GL_BLEND); 
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // use the normal blend function (source buffer over the frame buffer)
+
+    glm::mat4 projection = glm::ortho(0.0f, (float)SCR_WIDTH, 0.0f, (float)SCR_HEIGHT);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    // build and compile shaders
+    // -------------------------
+    Shader ourShader("/home/jjjurado/Dev/MyOpenGL/resources/shaders/glyphs.vs", "/home/jjjurado/Dev/MyOpenGL/resources/shaders/glyphs.fs");
+    ourShader.use();
+    ourShader.setMat4("projection", projection);
+
+    // load models
+    // -----------
+    
+    // draw in wireframe
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        processInput(window);
+
+
+        // view/projection transformations
+        // --------------------------
+
+        // render the loaded model
+        // -------------------------
+        RenderText(ourShader, "Hello World!", 10.0f, 200.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+GLenum glCheckError_(const char* file, int line)
+{
+    GLenum errorCode;
+    while((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+            case GL_INVALID_ENUM:                   error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                  error = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:              error = "INVALID_OPERATION"; break;
+            case GL_STACK_OVERFLOW:                 error = "STACK_OVERFLOW"; break;
+            case GL_STACK_UNDERFLOW:                error = "STACK_UNDERFLOW"; break;
+            case GL_OUT_OF_MEMORY:                  error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
+
+void APIENTRY glDebugOutput(GLenum source, 
+                            GLenum type, 
+                            unsigned int id, 
+                            GLenum severity, 
+                            GLsizei length, 
+                            const char *message, 
+                            const void *userParam)
+{
+    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; // ignore these non-significant error codes
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break; 
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+    
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+void RenderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    // activate corresponding render state
+    s.use();
+    glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for(c = text.begin(); c != text.end(); c++)
+    {
+        Character character = Characters[*c];
+        float xpos = x + character.Bearing.x * scale;
+        float ypos = y - (character.Size.y - character.Bearing.y) * scale;
+
+        float w = character.Size.x * scale;
+        float h = character.Size.y * scale;
+        float vertices[6][4] = {
+            {xpos,      ypos + h,       0.0f,   0.0f},
+            {xpos,      ypos,           0.0f,   1.0f},
+            {xpos + w,  ypos,           1.0f,   1.0f},
+
+            {xpos,      ypos + h,       0.0f,   0.0f},
+            {xpos + w,  ypos,           1.0f,   1.0f},
+            {xpos + w,  ypos + h,       1.0f,   0.0f},
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, character.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Advance cursors for next glyph (advance is 1/64 pixels)
+        x += (character.Advance >> 6) * scale; // bitshift by 6 ( 2⁶ = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
